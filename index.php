eeba9e4f1915522d968f0f377f4221ed76926d60
Martin de Jonge <madejo@hotmail.com>
2019-03-28 10:16:58 +0100

    v1.0

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+<body id="bg-img"> <!-- background-picture -->
+<div class="bg-overlay"></div>
+    <header>
+        <div class="menu-btn">
+            <div class="btn-line"></div>
+            <div class="btn-line"></div>
+            <div class="btn-line"></div>
+        </div>
+
+        <nav class="menu">
+            <div class="menu-branding">
+            </div>
+            <ul class="menu-nav">
+                <li class="nav-item">
+                    <a href="/" class="nav-link"></a>
+                        Home
+                </li>
+                <li class="nav-item">
+                    <a href="/about.html" class="nav-link"></a>
+                        About
+                </li>
+                <li class="nav-item">
+                    <a href="/portfolio.html class="nav-link"></a>
+                        My Portfolio
+                </li>
+                <li class="nav-item">
+                    <a href="/contact.html" class="nav-link"></a>
+                        How to reach me
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main id="home">
+        <h1 class="lg-heading">Martin 
+             <span class="text-secondary">de</span> Jonge
+        </h1>
+        <h2 class="sm-heading">
+            Web-Developer in training
+        </h2>
+        <div class="icons">
+            <a href="#">
+                <i class="fab fa-linkedin fa-7x"></i>  <!-- fab is style code of font awesome -->
+            </a>
+            <a href="#">
+              <i class="fab fa-github fa-7x"></i>   
+            </a>
+        </div>
+
+    </main>
+    <script src="js/main.js"></script>
+</body>
+</html>
