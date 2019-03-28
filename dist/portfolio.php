c86cbdd9976d566fb7b5c06965117d83f6538ff4
Martin de Jonge <madejo@hotmail.com>
2019-03-28 14:59:43 +0100

    v1.5

---
+++ dist/portfolio.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"">
+    <link rel="stylesheet" href="../dist/css/main.css" />
+    <title>My Portfolio</title>
+  </head>
+  <body>
+    <main id="portfolio">
+      <h1 class="lg-heading">
+        My
+        <span class="text-secondary">Portfolio</span>
+      </h1>
+
+      <h2 class="sm-heading">
+        Please check out some of my Projects ....
+      </h2>
+      
+      <div class="projects">
+          <div class="project">
+            <a href="#">
+                <img src="../dist/img/project1.jpg" alt="project">
+            </a>
+            <a href="" class="btn-light">
+            <i class="fas fa-eye"></i>Project     
+            </a>          
+            <a href="" class="btn-dark">
+            <i class="fab fa-github"></i>Github     
+            </a>          
+            </div>
+      
+            <div class="project">
+            <a href="#">
+                <img src="../dist/img/project2.jpg" alt="project">
+            </a>
+            <a href="" class="btn-light">
+            <i class="fas fa-eye"></i>Project     
+            </a>          
+            <a href="" class="btn-dark">
+            <i class="fab fa-github"></i>Github     
+            </a>          
+            </div>
+      
+            <div class="project">
+            <a href="#">
+                <img src="../dist/img/project3.jpg" alt="project">
+            </a>
+            <a href="" class="btn-light">
+            <i class="fas fa-eye"></i>Project     
+            </a>          
+            <a href="" class="btn-dark">
+            <i class="fab fa-github"></i>Github     
+            </a>          
+            </div>
+      
+            <div class="project">
+            <a href="#">
+                <img src="../dist/img/project4.jpg" alt="project">
+            </a>
+            <a href="" class="btn-light">
+            <i class="fas fa-eye"></i>Project     
+            </a>          
+            <a href="" class="btn-dark">
+            <i class="fab fa-github"></i>Github     
+            </a>          
+            </div>
+      
+            <div class="project">
+            <a href="#">
+                <img src="../dist/img/project5.jpg" alt="project">
+            </a>
+            <a href="" class="btn-light">
+            <i class="fas fa-eye"></i>Project     
+            </a>          
+            <a href="" class="btn-dark">
+            <i class="fab fa-github"></i>Github     
+            </a>          
+            </div>
+
+      </div>
+    </main>
+
+    <footer id="main-footer">
+      Copyright &copy; 2019
+    </footer>
+
+    <script src="js/main.js"></script>
+  </body>
+</html>
